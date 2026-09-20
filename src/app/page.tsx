3253c49f15530a2f5874582978b3985b690407b6
JorellanaF <jorellanafdev@gmail.com>
2026-09-19 23:06:59 -0600

    feat: muestra el listado de productos desde un server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import { getCategories, getProducts } from "@/lib/api";
+
+export default async function Home() {
+  const [products, categories] = await Promise.all([
+    getProducts(),
+    getCategories(),
+  ]);
   return (
     <main>
       <h1>Catálogo</h1>
+      <h2>Productos</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.title}</h3>
+            <p>{product.price}</p>
+          </li>
+        ))}
+      </ul>
+      <h2>Categorías:</h2>
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>
+            <h3>{category}</h3>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
